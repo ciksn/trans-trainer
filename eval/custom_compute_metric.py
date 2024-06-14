9b721ad1ad763368ea2e525300f3329ac16ed580
panzeyu2013 <1971614652@qq.com>
2024-06-14 20:30:12 +0800

    add custom metric

--- eval/custom_compute_metric.py
+++ eval/custom_compute_metric.py
@@ -1,1 +1,13 @@
-#TODO important for intergration!!
+import numpy as np
+from typing import Optional
+from transformers.tokenization_utils import PreTrainedTokenizer
+from ..eval.eval_pack.eval import language_eval
+
+def custom_compute_metric(eval_input,tokenizer: Optional[PreTrainedTokenizer] = None):
+    #pred -> batch of single sentences -> ndarry (B(stacked),T,V)
+    #labels -> batch of a group of reference sentences -> List[List[str],...]
+    logits, labels = eval_input
+    pred_idx = np.argmax(logits,axis=2) # (B,T,V)
+    pred = tokenizer.batch_decode(pred_idx)
+
+    return language_eval(pred,labels)
